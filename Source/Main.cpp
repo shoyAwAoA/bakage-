aab860a39c3972c40916eda1d55ce78860701562
shoyAwAoA <2230166@ecc.ac.jp>
2024-10-01 23:29:45 +0900

    あ

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -37,10 +37,10 @@ INT WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, LPWSTR cmd_line
 
 	RECT rc = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
 	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-	//HWND hWnd = CreateWindow(_T("Game"), _T(""), WS_OVERLAPPEDWINDOW ^ WS_MAXIMIZEBOX ^ WS_THICKFRAME | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, instance, NULL);
-	HWND hWnd = CreateWindow(_T("Game"), _T(""), WS_POPUP, CW_USEDEFAULT, CW_USEDEFAULT,
-	  GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
-	  NULL, NULL, instance, NULL);//ここのコメントを外して、上のやつをコメントにするとフルスクリーン
+	HWND hWnd = CreateWindow(_T("Game"), _T(""), WS_OVERLAPPEDWINDOW ^ WS_MAXIMIZEBOX ^ WS_THICKFRAME | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, instance, NULL);
+	//HWND hWnd = CreateWindow(_T("Game"), _T(""), WS_POPUP, CW_USEDEFAULT, CW_USEDEFAULT,
+	//  GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
+	//  NULL, NULL, instance, NULL);//ここのコメントを外して、上のやつをコメントにするとフルスクリーン
 	ShowWindow(hWnd, cmd_show);
 
 	Framework f(hWnd);
